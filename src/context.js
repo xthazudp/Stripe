f18aa493491b79b66097f78fdf5497ae5c1c8f56
Sudip <xthazudp@gmail.com>
2023-04-22 12:16:14 +0545

    adding context

--- src/context.js
+++ src/context.js
@@ -1,2 +1,44 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, createContext, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSiderbarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSiderbarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
